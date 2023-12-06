160e9bdc4c93035add0fd43eeafb9242538f59a7
Shreya Maheshwari <shreya.maheshwari@cubastion.com>
2023-12-06 11:58:59 +0530

    approver function jpn

--- approverfunction.js
+++ approverfunction.js
@@ -1,10 +1,24 @@
 class appnew{
-    constructor(page) { 
+    constructor(page,page1) { 
 
         this.position = page.locator('[aria-roledescription="Position"]');
+        this.positionjpn = page1.locator('[aria-roledescription="役職"]');
+
     }
         async isValidApprover(validApprovers,n) {
             const approverElement = await this.position.nth(n).textContent();
+            console.log(approverElement);
+            const isValid = validApprovers.includes(approverElement);
+            if (isValid) {
+              console.log("Correct approver as expected");
+            } else {
+              console.log("Wrong approver, current approver is "+approverElement+"correct approver is "+validApprovers);
+            }
+            return isValid;
+          }
+
+          async isValidApproverJPN(validApprovers,n) {
+            const approverElement = await this.positionjpn.nth(n).textContent();
             console.log(approverElement);
             const isValid = validApprovers.includes(approverElement);
             if (isValid) {
