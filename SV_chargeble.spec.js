72a42f08fd487a281862f154b3d8525eb8e45531
unknown <riya.sarkar@cubastion.com>
2023-12-06 11:47:59 +0530

    SV Verify

--- SV_chargeble.spec.js
+++ SV_chargeble.spec.js
@@ -12,13 +12,28 @@ test('record demo', async () => {
   test.setTimeout(1500000);
   const browser = await chromium.launch({
 
-    headless: true
+    headless: false
 
   });
   const context = await browser.newContext();
   const context1 = await browser.newContext();
+  const context2 = await browser.newContext();
+  const context3 = await browser.newContext();
+  const context4 = await browser.newContext();
+  const context5 = await browser.newContext();
+  const context6 = await browser.newContext();
 
   const page = await context.newPage();
+  const app1 = await context2.newPage();
+  const app2 = await context3.newPage();
+  const app3 = await context4.newPage();
+  const app4 = await context5.newPage();
+  const app5 = await context6.newPage();
+
+
+
+
+
 
   // const pageApp3 = await context3.newpage();
   await page.goto('https://forcefdp-rt2.mitsubishi-fuso.com/siebel/app/edealer/jpn?');
@@ -28,6 +43,46 @@ test('record demo', async () => {
   await page.click('#loginSubmitButton');
   await page.waitForLoadState();
   await page.waitForTimeout(3000);
+
+  await app1.goto('https://forcefdp-rt2.mitsubishi-fuso.com/siebel/app/edealer/jpn?');
+  await app1.locator('#userid').fill('D8FORF25');
+  await app1.click('#next-btn');
+  await app1.locator('#password').fill('Snakamura@1');
+  await app1.click('#loginSubmitButton');
+  await app1.waitForLoadState();
+  await app1.waitForTimeout(3000);
+
+  await app2.goto('https://forcefdp-rt2.mitsubishi-fuso.com/siebel/app/edealer/jpn?');
+  await app2.locator('#userid').fill('D8FDFO25');
+  await app2.click('#next-btn');
+  await app2.locator('#password').fill('Snakamura@1');
+  await app2.click('#loginSubmitButton');
+  await app2.waitForLoadState();
+  await app2.waitForTimeout(3000);
+
+  await app3.goto('https://forcefdp-rt2.mitsubishi-fuso.com/siebel/app/edealer/jpn?');
+  await app3.locator('#userid').fill('D8FFDP20');
+  await app3.click('#next-btn');
+  await app3.locator('#password').fill('Snakamura@1');
+  await app3.click('#loginSubmitButton');
+  await app3.waitForLoadState();
+  await app3.waitForTimeout(3000);
+
+  await app4.goto('https://forcefdp-rt2.mitsubishi-fuso.com/siebel/app/edealer/jpn?');
+  await app4.locator('#userid').fill('D8FDPF20');
+  await app4.click('#next-btn');
+  await app4.locator('#password').fill('Snakamura@1');
+  await app4.click('#loginSubmitButton');
+  await app4.waitForLoadState();
+  await app4.waitForTimeout(3000);
+
+  await app5.goto('https://forcefdp-rt2.mitsubishi-fuso.com/siebel/app/edealer/jpn?');
+  await app5.locator('#userid').fill('D8FDFO19');
+  await app5.click('#next-btn');
+  await app5.locator('#password').fill('Snakamura@1');
+  await app5.click('#loginSubmitButton');
+  await app5.waitForLoadState();
+  await app5.waitForTimeout(3000);
 
   await page.goto('https://forcefdp-rt2.mitsubishi-fuso.com/siebel/app/edealer/jpn?SWECmd=GotoView&SWEView=eAuto+All+Vehicle+View');
   await page.waitForLoadState();
@@ -88,13 +143,13 @@ test('record demo', async () => {
   await page.locator('[id="s_1_2_64_0_icon"]').click();
 
   await page.getByRole('button', { name: 'ピック労働コード リストアプレット:OK' }).click();
-
+  await page.pause()
   await page.getByRole('gridcell', { name: '計算機フィールド' }).first().click();
   await page.locator('[aria-roledescription="数量"]').click();
-  await page.locator('[name="Recommended_Quantity"]').fill('1');
+  await page.locator('[name="Recommended_Quantity"]').fill('100');
 
   await page.locator('[aria-roledescription="純作業時間"]').click();
-  await page.locator('[name="MF_Net_Operation_Time"]').fill('8');
+  await page.locator('[name="MF_Net_Operation_Time"]').fill('200');
   await page.locator('[name="MF_Net_Operation_Time"]').press('Control+s');
 
   await page.getByRole('link', { name: 'パーツ' }).click();
@@ -119,7 +174,81 @@ test('record demo', async () => {
   await page.locator('[name="Name"]').nth(0).click();
   //await page.goto('https://forcefdp-rt2.mitsubishi-fuso.com/siebel/app/edealer/jpn?SWECmd=GotoView&SWEView=MF+Service+Quote+Line+Items+View&SWERF=1&SWEHo=&SWEBU=1&SWEApplet0=MF+Service+Quote+Form+Applet&');
   await page.getByRole('button', { name: '見積り フォームアプレット:承認依頼' }).click();
-  await page.locator('#s_2_1_130_0_icon').click();
+  await page.getByRole('link', { name: '承認', exact: true }).click();
+  await page.waitForTimeout(2000);
+  await page.getByPlaceholder('見積番号').click();
+  await page.getByPlaceholder('見積番号').press('Alt+Control+k');
+  await page.getByLabel('ロウ番号').click();
+  const rowid1 = await page.getByLabel('ロウ番号').textContent();
+  console.log(rowid1)
+  await page.getByLabel('レコード情報 フォームアプレット:OK').click();
+//Verify Approval started
+  const validApprovers = ["Branch-Service-Mgr", "Branch-Head", "SCHQ-CS-Service-Mgr", "SCHQ-CS-Snr-Mgr", "HQ-Finance-GMgr"];
+  const verfyappvr = require('./approverfunction');
+    //initiating the constructor
+  const SalesGPStaff = new verfyappvr.appnew(page);
+  for (let n = 0; n < validApprovers.length; n++) {
+      const isApproverValid = await SalesGPStaff.isValidApprover(validApprovers[n],n);
+  }
+  //Verify Approval end
+
+  //Approval started
+    await app1.goto('https://forcefdp-rt2.mitsubishi-fuso.com/siebel/app/edealer/enu?SWECmd=GotoView&SWEView=UInbox+My+Team+Inbox+Item+List+View',{ waitUntil: 'networkidle' });
+    await app1.bringToFront();
+    await app1.locator('[aria-label="Inbox Items List Applet:Query"]').click();
+    await app1.locator('[id="1_s_1_l_Name"]').click();
+    await app1.locator('[id="1_Name"]').fill(rowid1);
+    await app1.locator('[aria-label="Inbox Items List Applet:Go"]').click();
+    await app1.locator('[id="1_s_1_l_Action"]').click();
+    await app1.locator('[id="1_Action"]').fill('Approved');
+    await app1.locator('[id="1_Action"]').press('Control+s');
+    await app1.waitForLoadState('networkidle');
+
+    await app2.goto('https://forcefdp-rt2.mitsubishi-fuso.com/siebel/app/edealer/enu?SWECmd=GotoView&SWEView=UInbox+My+Team+Inbox+Item+List+View',{ waitUntil: 'networkidle' });
+    await app2.bringToFront();
+    await app2.locator('[aria-label="Inbox Items List Applet:Query"]').click();
+    await app2.locator('[id="1_s_1_l_Name"]').click();
+    await app2.locator('[id="1_Name"]').fill(rowid1);
+    await app2.locator('[aria-label="Inbox Items List Applet:Go"]').click();
+    await app2.locator('[id="1_s_1_l_Action"]').click();
+    await app2.locator('[id="1_Action"]').fill('Approved');
+    await app2.locator('[id="1_Action"]').press('Control+s');
+    await app2.waitForLoadState('networkidle');
+
+    await app3.goto('https://forcefdp-rt2.mitsubishi-fuso.com/siebel/app/edealer/enu?SWECmd=GotoView&SWEView=UInbox+My+Team+Inbox+Item+List+View',{ waitUntil: 'networkidle' });
+    await app3.bringToFront();
+    await app3.locator('[aria-label="Inbox Items List Applet:Query"]').click();
+    await app3.locator('[id="1_s_1_l_Name"]').click();
+    await app3.locator('[id="1_Name"]').fill(rowid1);
+    await app3.locator('[aria-label="Inbox Items List Applet:Go"]').click();
+    await app3.locator('[id="1_s_1_l_Action"]').click();
+    await app3.locator('[id="1_Action"]').fill('Approved');
+    await app3.locator('[id="1_Action"]').press('Control+s');
+    await app3.waitForLoadState('networkidle');
+
+    await app4.goto('https://forcefdp-rt2.mitsubishi-fuso.com/siebel/app/edealer/enu?SWECmd=GotoView&SWEView=UInbox+My+Team+Inbox+Item+List+View',{ waitUntil: 'networkidle' });
+    await app4.bringToFront();
+    await app4.locator('[aria-label="Inbox Items List Applet:Query"]').click();
+    await app4.locator('[id="1_s_1_l_Name"]').click();
+    await app4.locator('[id="1_Name"]').fill(rowid1);
+    await app4.locator('[aria-label="Inbox Items List Applet:Go"]').click();
+    await app4.locator('[id="1_s_1_l_Action"]').click();
+    await app4.locator('[id="1_Action"]').fill('Approved');
+    await app4.locator('[id="1_Action"]').press('Control+s');
+    await app4.waitForLoadState('networkidle');
+
+    await app5.goto('https://forcefdp-rt2.mitsubishi-fuso.com/siebel/app/edealer/enu?SWECmd=GotoView&SWEView=UInbox+My+Team+Inbox+Item+List+View',{ waitUntil: 'networkidle' });
+    await app5.bringToFront();
+    await app5.locator('[aria-label="Inbox Items List Applet:Query"]').click();
+    await app5.locator('[id="1_s_1_l_Name"]').click();
+    await app5.locator('[id="1_Name"]').fill(rowid1);
+    await app5.locator('[aria-label="Inbox Items List Applet:Go"]').click();
+    await app5.locator('[id="1_s_1_l_Action"]').click();
+    await app5.locator('[id="1_Action"]').fill('Approved');
+    await app5.locator('[id="1_Action"]').press('Control+s');
+    await app5.waitForLoadState('networkidle');
+
+  await page.locator('#s_2_1_130_0_icon').click(); 
   await page.getByRole('button', { name: '担当者を選択 リストアプレット:OK' }).click();
   await page.getByPlaceholder('見積状況').click();
 
