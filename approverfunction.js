d65e8493d0a185520950b00eeb156362db6068ce
Amit Kumar <Amit.Mishra@cubastion.com>
2023-12-11 18:19:22 +0530

    Changes made in UV Assessment

--- approverfunction.js
+++ approverfunction.js
@@ -1,9 +1,10 @@
 class appnew{
-    constructor(page) { 
-
+  constructor(page) {
+    this.page = page;
+ 
         this.position = page.locator('[aria-roledescription="Position"]');
         this.positionjpn = page.locator('[aria-roledescription="役職"]');
-
+ 
     }
         async isValidApprover(validApprovers,n) {
             const approverElement = await this.position.nth(n).textContent();
@@ -16,7 +17,7 @@ class appnew{
             }
             return isValid;
           }
-
+   
           async isValidApproverJPN(validApprovers,n) {
             const approverElement = await this.positionjpn.nth(n).textContent();
             console.log(approverElement);
@@ -28,5 +29,29 @@ class appnew{
             }
             return isValid;
           }
+ 
+ 
+        async correctApprover(rowid){
+            await this.page.locator('[aria-label="Inbox Items List Applet:Query"]').click();
+            await this.page.locator('[id="1_s_1_l_Name"]').click();
+            await this.page.locator('[id="1_Name"]').fill(rowid);
+            await this.page.locator('[aria-label="Inbox Items List Applet:Go"]').click();
+            await this.page.locator('[id="1_s_1_l_Action"]').click();
+            await this.page.locator('[id="1_Action"]').fill('Approved');
+            await this.page.locator('[id="1_Action"]').press('Control+s');
+            await this.page.waitForLoadState('networkidle');          
+ 
     }
+    async correctApproverJPN(rowid){
+      await this.page.locator('[id="s_2_1_10_0_Ctrl"]').click();
+      await this.page.locator('[id="1_s_2_l_Name"]').click();
+      await this.page.locator('[id="1_Name"]').fill(rowid);
+      await this.page.locator('[id="s_2_1_7_0_Ctrl"]').click();
+      await this.page.locator('[id="1_s_2_l_Action"]').click();
+      await this.page.locator('[id="1_Action"]').fill('承認');
+      await this.page.locator('[id="1_Action"]').press('Control+s');
+      await this.page.waitForLoadState('networkidle');          
+ 
+}
+  }
 module.exports = {appnew};
